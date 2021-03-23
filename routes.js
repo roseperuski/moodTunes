db2e8cc73edbd2aa65b6534a79d3a633e412a9c6
Kimberly Hicks <kim@Hicks-MBP.attlocal.net>
2021-03-22 22:01:08 -0400

    POST works

--- routes.js
+++ routes.js
@@ -1,58 +1,54 @@
-const { json } = require('body-parser');
-const express = require('express');
+const { json } = require("body-parser");
+const express = require("express");
 const tracks = express.Router();
 const pool = require("./connection");
 
 tracks.get("/", (req, res) => {
-
-    console.log('entering get tracks routes');
-    pool.query("SELECT * FROM playlist")
-    .then((results) => { // do this when we get the result
+  console.log("entering get tracks routes");
+  pool.query("SELECT * FROM playlist").then((results) => {
+    // do this when we get the result
     // make the DB data match the front end format
-   let rtnTracks = results.rows;
-   
-  if (!rtnTracks){
-    res.status(404).send('Empty search list!!');
-  }
-  else{
-    res.status(200).json(rtnTracks);
-  }
-  
+    let rtnTracks = results.rows;
+
+    if (!rtnTracks) {
+      res.status(404).send("Empty search list!!");
+    } else {
+      res.status(200).json(rtnTracks);
+    }
+  });
 });
-});
+
 // accept POST request at URI: /items
 
-// tracks.post("/", (req, res) => {
-//   let product = req.body.product;
-//   let price = req.body.price;
-//   let quantity= req.body.quantity;
-//   console.log('req:',req.body);
-// pool
-// .query(
-//   `
-// INSERT INTO 
-//     shopping_cart(product, price, quantity) 
-//     VALUES ($1, $2, $3) returning *
-// `,
-//   [
-//     artist,
-//     name,
-//     Image
-//   ]
-// )
-// .then((results) => {
-//   res.status(201); // created
-//   res.json(results.rows); // return the item we created
-// });
-   
-   
-// });
+tracks.post("/", (req, res) => {
+  let newTrack = req.body;
+  // let artist = req.body.artist;
+  // let name = req.body.name;
+  // let artistUrl = req.body.urlArtist;
+  // let trackUrl = req.body.urlTrack;
+  
+  console.log("req:", newTrack);
+  
+  pool.query('INSERT INTO playlist (artist_name, track_name, artist_url, track_url) VALUES ($1, $2, $3, $4) returning *;',
+  [newTrack.artist_name, newTrack.track_name, newTrack.artist_url, newTrack.track_url
+  ]).then((results) => {
+      res.status(201); // created
+      res.json(results.rows); // return the item we created
+  });
+});
 
-// // accept PUT request at URI:  
+// 'INSERT INTO
+//   playlist (artist_name, track_name, artist_url, track_url)
+//     VALUES ($1, $2, $3,$4) returning *;',
+//       [newTrack.artist, newTrack.name, newTrack.artistUrl, newTrack.trackUrl]
+
+
+
+// // accept PUT request at URI:
 // tracks.put("/:id", (req, res) => {
 //   const idInp = (req.params.id);
 //   console.log("id:",idInp);
-  
+
 //   let product = req.body.product;
 //   let price = req.body.price;
 //   let quantity= req.body.quantity;
@@ -62,17 +58,17 @@ tracks.get("/", (req, res) => {
 //   pool
 // .query(
 //   `
-// update 
-//     shopping_cart set product = $1, price = $2, quantity = $3 
+// update
+//     shopping_cart set product = $1, price = $2, quantity = $3
 //     where id = $4 returning *
-    
+
 // `,
 //   [
 //     product,
 //     price,
 //     quantity,
 //     idInp
-//   ] 
+//   ]
 // )
 // .then((results) => {
 //   res.status(201); // created
@@ -101,21 +97,19 @@ tracks.get("/", (req, res) => {
 //   pool
 //   .query(
 //     `
-//   delete from 
+//   delete from
 //       shopping_cart where id = $1
 // `,
 //     [
 //       idDel
-//     ] 
+//     ]
 //   )
 //   .then((results) => {
 //     res.status(201); // created
 //     res.json(results); // return the item we created
 //   });
 
-
 // });
-
 
 //export module so it can be used in other files
 module.exports = tracks;
